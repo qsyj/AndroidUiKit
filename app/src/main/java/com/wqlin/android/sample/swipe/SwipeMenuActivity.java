aa6fcd731ec06e88dd1a488d9e8ccaee92111b34
wangql <wangql@leleyuntech.com>
2017-10-25 15:58:46 +0800

    添加侧滑布局SwipeMenuLayout

---
+++ app/src/main/java/com/wqlin/android/sample/swipe/SwipeMenuActivity.java
@@ -0,0 +1,20 @@
+package com.wqlin.android.sample.swipe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wqlin.android.sample.R;
+
+/**
+ * @author wangql
+ * @email wangql@leleyuntech.com
+ * @date 2017/10/25 14:21
+ */
+public class SwipeMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_menu);
+    }
+}
