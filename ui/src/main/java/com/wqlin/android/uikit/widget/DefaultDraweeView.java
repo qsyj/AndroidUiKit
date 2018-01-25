ce88be5ea356017c9e99cc74c4e0d76bff2d781b
wangql <wangql@leleyuntech.com>
2018-01-25 14:09:05 +0800

    1.version = "0.0.9"	//aar的版本号; 2.DefaultDraweeView优化;

--- ui/src/main/java/com/wqlin/android/uikit/widget/DefaultDraweeView.java
+++ ui/src/main/java/com/wqlin/android/uikit/widget/DefaultDraweeView.java
@@ -2,8 +2,10 @@ package com.wqlin.android.uikit.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
@@ -16,25 +18,28 @@ import com.wqlin.android.uikit.R;
 /**
  * PlaceholderImage默认实现占位图 使用R.drawable.default_drawee_logo
  * <p>
- * 属性iconWidth-->default_logo的width 默认30dp
+ *  占位图的默认R.drawable.default_drawee_logo
  * <p>
- * 属性iconHeight-->default_logo的height 默认30dp
+ * 属性iconWidth-->default_logo的width 默认26dp
  * <p>
- * 属性iconBackgroudColor-->default_logo的背景色 默认#cccccc
+ * 属性iconHeight-->default_logo的height 默认20dp
  * <p>
- * 属性iconResId-->占位图的资源id 默认R.drawable.default_drawee_logo
+ * 属性iconBackgroudColor-->default_logo的背景色 默认#e7e7e7
+ * <p>
+ * {@link #setIcon(int, float, float)}-->修改占位图 图片 图片大小 背景
  * @author wangql
  * @email wangql@leleyuntech.com
  * @date 2018/1/5 10:34
  */
 public class DefaultDraweeView extends SimpleDraweeView {
-    private final int defaultColor = Color.parseColor("#cccccc");
-    private final float defaultWidthDP = 30;
-    private final float defaultHeightDP = 30;
+    private final int defaultColor = Color.parseColor("#e7e7e7");
+    private final float defaultWidthDP = 26;
+    private final float defaultHeightDP = 20;
     private final int defaltIconResId = R.drawable.default_drawee_logo;
     private int mBackgroudColor=defaultColor;
     private float mBitmapWidth, mBitmapHeight;
     private int mIconResId;
+    private Drawable mIconDrawable;
 
     public DefaultDraweeView(Context context) {
         super(context);
@@ -60,10 +65,23 @@ public class DefaultDraweeView extends SimpleDraweeView {
         init(context,attrs);
     }
 
+    /**
+     *
+     * @param iconBackgroudColor 背景色 默认#e7e7e7
+     * @param iconWidth 占位图 logo的width 默认26dp
+     * @param iconHeight 占位图 logo的hegiht 默认26dp
+     */
     public void setIcon(int iconBackgroudColor,float iconWidth,float iconHeight) {
         setIcon(iconBackgroudColor,0,iconWidth,iconHeight);
     }
 
+    /**
+     *
+     * @param iconBackgroudColor 背景色 默认#e7e7e7
+     * @param iconResId 占位图的资源id 默认R.drawable.default_drawee_logo
+     * @param iconWidth 占位图 logo的width 默认26dp
+     * @param iconHeight 占位图 logo的hegiht 默认26dp
+     */
     public void setIcon(int iconBackgroudColor,int iconResId,float iconWidth,float iconHeight) {
         if (iconResId>0)
             mIconResId = iconResId;
@@ -86,7 +104,7 @@ public class DefaultDraweeView extends SimpleDraweeView {
             mBackgroudColor = a.getColor(R.styleable.DefaultDraweeView_iconBackgroudColor, Color.parseColor("#cccccc"));
             mBitmapWidth = a.getDimensionPixelSize(R.styleable.DefaultDraweeView_iconWidth, (int) mBitmapWidth);
             mBitmapHeight = a.getDimensionPixelSize(R.styleable.DefaultDraweeView_iconHeight, (int) mBitmapHeight);
-            mIconResId = a.getResourceId(R.styleable.DefaultDraweeView_iconResId, defaltIconResId);
+//            mIconDrawable = a.getDrawable(R.styleable.DefaultDraweeView_iconPlaceholderResId);
             a.recycle();
         }
         setHierarchy(context);
@@ -111,7 +129,15 @@ public class DefaultDraweeView extends SimpleDraweeView {
     }
 
     public Drawable getDefaultDrawable(Context context, DefaultDrawable.Params params) {
-        return new DefaultDrawable(BitmapFactory.decodeResource(context.getResources(), mIconResId),params);
+        Bitmap bitmap ;
+        if (mIconDrawable != null) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) mIconDrawable;
+            bitmap = bitmapDrawable.getBitmap();
+        } else {
+            bitmap = BitmapFactory.decodeResource(context.getResources(),mIconResId);
+        }
+        mIconDrawable = null;
+        return new DefaultDrawable(bitmap,params);
     }
 
     /**
